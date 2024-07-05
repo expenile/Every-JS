1c2b4b3fa3e0adb9e87c79ff34239295dd18be5e
Nilesh <expenilesh31@gmail.com>
2024-07-05 16:48:29 +0530

    async-await

---
+++ async-await.js
@@ -0,0 +1,10 @@
+/*
+ Blocking code vs Non-blocking code
+
+
+Blocking code:
+- Blocking code is synchronous code that blocks the execution of code until the operation completes.  --> Read file synchronously
+Non-blocking code:
+- Non-blocking code is asynchronous code that doesn't block the execution of code. --> Read file asynchronously 
+
+ */
